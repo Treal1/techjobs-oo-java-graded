d46c5aa7fd67f92e24adec2cbf3b7ec99e5c9fdb
Torrie Real <torijames1997@gmail.com>
2022-09-25 16:09:56 -0500

    -m "wrote custom string and the custom equals and hashcode methods"

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -17,9 +17,23 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
+ public String toString(){ return value; }
+//        public String toString(){
+//        return value;
+// }
+
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null|| getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return getId() == that.getId();
+
+    }
 
     // Getters and Setters:
 
@@ -27,9 +41,9 @@ public class PositionType {
         return id;
     }
 
-    public String getValue() {
-        return value;
-    }
+//    public String getValue() {
+//        return value;
+//    }
 
     public void setValue(String value) {
         this.value = value;
